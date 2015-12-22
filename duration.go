fb8b764c6ec7265ca03967089c404fe73baeef31
Ulric Qin <ulric.qin@gmail.com>
2015-12-22 15:18:16 +0800

    add duration

---
+++ duration.go
@@ -0,0 +1,34 @@
+package time
+
+import (
+	"fmt"
+)
+
+func HumanDuration(now, before int64) string {
+	d := now - before
+	if d <= 60 {
+		return "just now"
+	}
+
+	if d <= 120 {
+		return "1 minute ago"
+	}
+
+	if d <= 3600 {
+		return fmt.Sprintf("%d minutes ago", d/60)
+	}
+
+	if d <= 7200 {
+		return "1 hour ago"
+	}
+
+	if d <= 3600*24 {
+		return fmt.Sprintf("%d hours ago", d/3600)
+	}
+
+	if d <= 3600*24*2 {
+		return "1 day ago"
+	}
+
+	return fmt.Sprintf("%d days ago", d/3600/24)
+}
